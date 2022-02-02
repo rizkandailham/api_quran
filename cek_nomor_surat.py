af9df3ffa075d58c1071558c8717ffa8d053a09f
Ilham Rizkanda <rizkandailham@gmail.com>
2022-02-02 17:23:44 +0700

    cek nomor surat lalu tulis di csv

--- cek_nomor_surat.py
+++ cek_nomor_surat.py
@@ -1,6 +1,7 @@
 import requests
 import numpy as np
 import json
+import csv
 
 data=requests.get("https://equran.id/api/surat")
 data=json.loads(data.text)
@@ -8,5 +9,13 @@ data=json.loads(data.text)
 #data=np.array(data)
 jumlah_surat=0
 jumlah_ayat=0
-for i in data:
-    print(str(i["nomor"])+":"+i["nama_latin"])
+header=["Nomor","Nama Surat"]
+with open('surat.csv', 'w', encoding='UTF8') as f:
+    tulis=csv.writer(f)
+    tulis.writerow(header)
+    for i in data:
+            isi=[i['nomor'],i['nama_latin']]
+            tulis.writerow(isi)
+
+#for i in data:
+#    print(str(i["nomor"])+":"+i["nama_latin"])
