f64aa4f2888b2abb52a2e1cdcb2461608c872bf6
Ilham Rizkanda <rizkandailham@gmail.com>
2022-02-04 08:10:09 +0700

    jadiin class

--- simpen_ke_csv.py
+++ simpen_ke_csv.py
@@ -9,35 +9,37 @@ import pandas as pd
 #teks=np.array(teks)
 #print(teks['jumlah_ayat'])
 #np.savetxt('surat al fatihah.csv',teks,delimiter=',')
+class scrapper_api:
+    def __init__():
 
-def konversi_dict(teks):
-    teks=list(teks.items())
-    teks=np.array(teks)
-    return teks
+    def konversi_dict(teks):
+        teks=list(teks.items())
+        teks=np.array(teks)
+        return teks
 
-def ambil_isi(url):
-    r=requests.get(url)
-    teks=json.loads(r.text)
-    tmp=[]
-    for i in teks:
-        tmp.append(teks[i])
-    return tmp
+    def ambil_isi(url):
+        r=requests.get(url)
+        teks=json.loads(r.text)
+        tmp=[]
+        for i in teks:
+            tmp.append(teks[i])
+        return tmp
 
-def lihat_index(url):
-    r=requests.get(url)
-    teks=json.loads(r.text)
-    bit_simpen=[]
-    for i in teks:
-        bit_simpen.append(i)
-    print(*bit_simpen, sep="\n")
+    def lihat_index(url):
+        r=requests.get(url)
+        teks=json.loads(r.text)
+        bit_simpen=[]
+        for i in teks:
+            bit_simpen.append(i)
+        print(*bit_simpen, sep="\n")
 
-def simpan_ke_csv(url):
-    r=requests.get(url)
-    teks=r.text
-    teks=json.loads(teks)
-    data=list(teks.items())
-    teks=np.array(data)
-    df=pd.DataFrame(teks)
-    df.to_csv("Daftar Surat.csv")
+    def simpan_ke_csv(url):
+        r=requests.get(url)
+        teks=r.text
+        teks=json.loads(teks)
+        data=list(teks.items())
+        teks=np.array(data)
+        df=pd.DataFrame(teks)
+        df.to_csv("Daftar Surat.csv")
 
-ambil_isi(teks)
+#ambil_isi(teks)
